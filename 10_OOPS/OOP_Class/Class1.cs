990ead0dd7ea8ba925198dcadfc4df0be2df43dd
classintown <classintown@gmail.com>
2024-10-03 18:59:38 +0530

    Create Objects

---
+++ 10_OOPS/OOP_Class/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace OOP_Class;
+
+public class Class1
+{
+    public int x = 10;
+}
---
+++ 10_OOPS/OOP_Console/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OOP_Class;
+
+Class1 c = new Class1();
+Console.WriteLine(c.x);
